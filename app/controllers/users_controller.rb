3368b32aeaa992fbe53347dbeba8f3bbcd7e2577
huy thuy <jbhuythuy@gmail.com>
2018-07-27 13:31:05 +0700

    init project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    debugger
+  def index
+     @users = User.all 
   end
-
+ 
   def new
+      @user=User.new
   end
+  def create
+  @user = User.new user_params
+  if @user.save
+    flash[:success] = "Register success"
+    redirect_to users_path
+  else
+    flash[:error] = "Register failed"
+    render 'new'
+  end
+end
+
+private
+def user_params
+  params.require(:user).permit :name,:email, :password, :password_confirmation
+end
 end
